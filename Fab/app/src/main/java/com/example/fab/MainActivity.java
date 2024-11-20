55fb5402eee549e0b0949540b90777bbd6f28cdd
MrQbert <65645542+MrQbert@users.noreply.github.com>
2024-11-20 12:36:32 -0600

    Commit 2
    making 2 more buttons that appear and disappear on click

--- Fab/app/src/main/java/com/example/fab/MainActivity.java
+++ Fab/app/src/main/java/com/example/fab/MainActivity.java
@@ -6,26 +6,79 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-    FloatingActionButton mainfab;
-    TextView mainText;
+    FloatingActionButton mainfab, addfab, deletefab;
+    TextView mainText, addText, deleteText;
+    Boolean flag;
+    Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent i = new Intent(this, secondActivity.class);
+        i = new Intent(this, secondActivity.class);
 
         mainfab = findViewById(R.id.mainIcon);
         mainText = findViewById(R.id.mainText);
+        addfab = findViewById(R.id.addIcon);
+        deletefab = findViewById(R.id.deleteIcon);
+        addText = findViewById(R.id.addText);
+        deleteText = findViewById(R.id.deleteText);
+        flag = false;
+        addfab.setVisibility(View.GONE);
+        addText.setVisibility(View.GONE);
+        deletefab.setVisibility(View.GONE);
+        deleteText.setVisibility(View.GONE);
+
+        addfab.setOnClickListener(new add());
+        deletefab.setOnClickListener(new delete());
+
 
         mainfab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(i);
+                if(!flag){
+                    addfab.show();
+                    deletefab.show();
+                    addText.setVisibility(View.VISIBLE);
+                    deleteText.setVisibility(View.VISIBLE);
+                    mainText.setVisibility(View.GONE);
+                    flag= true;
+                }else{
+                    addfab.hide();
+                    deletefab.hide();
+                    addText.setVisibility(View.GONE);
+                    deleteText.setVisibility(View.GONE);
+                    mainText.setVisibility(View.VISIBLE);
+                    flag= false;
+                }
             }
         });
+
+
+//        mainfab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(i);
+//            }
+//        });
+    }
+
+     public class add implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText( getApplicationContext(), "This Works",Toast.LENGTH_SHORT).show();
+
+        }
+    }
+    public class delete implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText( getApplicationContext(), "This Works",Toast.LENGTH_SHORT).show();
+           startActivity(i);
+        }
     }
 }
